d87d7c703f4b5b1d19b74164f531a2344450b7f2
Moka-Austine <141416002+Moka-Austine@users.noreply.github.com>
2025-01-21 19:10:09 +0300

    Update shuffling.java

--- SAFARICOM_APP/shuffling.java
+++ SAFARICOM_APP/shuffling.java
@@ -31,73 +31,89 @@ public class shuffling {
 
         String zero_seven_1 = zero_seven_or_one + option;
 
-        if (shuffled_mobile_num1_ans.equals("yes") || shuffled_mobile_num1_ans.equals("y")) {
+        switch(shuffled_mobile_num1_ans) {
 
-         System.out.print("Please enter your username: " );
-         String user_account_name = prompt_user.nextLine();
+            //? if shuffled_mobile_num1_ans is equal to yes
+            case "yes", "y" -> {
 
-         System.out.print("Please enter your ID number: " );
-         String user_id_num = prompt_user.nextLine();
+                System.out.print("Please enter your username: " );
+                String user_account_name = prompt_user.nextLine();
 
-         System.out.print("Please enter a 4 digit pin account pin to use the mobile number: " );
-         String user_mobile_num_pin = prompt_user.nextLine();
-         
+                System.out.print("Please enter your ID number: " );
+                String user_id_num = prompt_user.nextLine();
 
-         System.out.print("Please re-enter your pin again to confirm sighning up to your Safaricom account: ");
-         String user_mobile_num_pin_confirm = prompt_user.nextLine();
-         
-         //updating user info
-         String[] data_updates = DATA_UPDATE.updateUserInfo(user_account_name, user_mobile_num_pin, zero_seven_1, user_id_num);
+                System.out.print("Please enter a 4 digit pin account pin to use the mobile number: " );
+                String user_mobile_num_pin = prompt_user.nextLine();
+                
 
-         if (user_mobile_num_pin_confirm.equals(user_mobile_num_pin)) {
-             
-             // safaricom services
-             INFINITE_WHILE_LOOP.INFINITE_WHILE_LOOP(data_updates, PERSONAL_INFO.agentInfor);
-             
-         }  
-         else {
-             System.out.print("Wrong pin, 2 times remaining. Please reenter again: ");
-             String ans = prompt_user.nextLine();
-             if (ans.equals(user_mobile_num_pin)) {
-                INFINITE_WHILE_LOOP.INFINITE_WHILE_LOOP(data_updates, PERSONAL_INFO.agentInfor);
-             }
-            else{
-                System.out.print("Wrong pin, 1 times remaining. Please reenter again: ");
-                String ans2 = prompt_user.nextLine();
-                if (ans2.equals(user_mobile_num_pin)) {
-                    INFINITE_WHILE_LOOP.INFINITE_WHILE_LOOP(data_updates, PERSONAL_INFO.agentInfor);
-                 }
-                 else{
-                    System.out.print("You have reentered wrong pin 3 times, your account has been blocked from any access. Please carry your ID card and visit our Safaricom headquaters near you to assist you unlock your account.");
+                System.out.print("Please re-enter your pin again to confirm sighning up to your Safaricom account: ");
+                String user_mobile_num_pin_confirm = prompt_user.nextLine();
+                
+                //updating user info
+                String[] data_updates = DATA_UPDATE.updateUserInfo(user_account_name, user_mobile_num_pin, zero_seven_1, user_id_num);
+
+                if (user_mobile_num_pin_confirm.equals(user_mobile_num_pin)) {
                     
+                    // safaricom services
+                    INFINITE_WHILE_LOOP.INFINITE_WHILE_LOOP(data_updates, PERSONAL_INFO.agentInfor);
+                    
+                }  
+                else {
+                    System.out.print("Wrong pin, 2 times remaining. Please reenter again: ");
+                    String ans = prompt_user.nextLine();
+                    if (ans.equals(user_mobile_num_pin)) {
+                        INFINITE_WHILE_LOOP.INFINITE_WHILE_LOOP(data_updates, PERSONAL_INFO.agentInfor);
+                    }
+                    else{
+                        System.out.print("Wrong pin, 1 times remaining. Please reenter again: ");
+                        String ans2 = prompt_user.nextLine();
+                        if (ans2.equals(user_mobile_num_pin)) {
+                            INFINITE_WHILE_LOOP.INFINITE_WHILE_LOOP(data_updates, PERSONAL_INFO.agentInfor);
+                        }
+                        else{
+                            System.out.print("You have reentered wrong pin 3 times, your account has been blocked from any access. Please carry your ID card and visit our Safaricom headquaters near you to assist you unlock your account.");
+                            
+                        }
+                    }
                 }
+                break;
             }
-         }
 
+            //? if shuffled_mobile_num1_ans is equal to no
+            case "no", "n" -> {
+               
+                //no answer an ask if to shuffle or choose option 2 & 3 or cancel
+                //also add options for y/n just not only yes/no
+                
+                System.out.print("Do you want to choose option 2, 3 or cancel account? ( 2, 3 ) or c to cancel: ");
+                String ans = prompt_user.nextLine();
+
+
+                switch(ans) {
+                    case "2" -> {
+                        System.out.print("Are you sure to choose " + zero_seven_or_one + option + " ? (Yes / No) OR (y/n)" );
+                        break;
+                    }
+                    case "3" -> {
+                        //implement third option
+                        break;
+                    }
+
+                    default -> {
+                        //implement cancel account optionz
+                    }
+
+                }
+                
+                break;
+            }
+
+            default -> {
+                System.out.println("Invalid choice, please try again: ");
+            }
         }
+
         
-        else if (shuffled_mobile_num1_ans.equals("no") || shuffled_mobile_num1_ans.equals("n")) {
-         //no answer an ask if to shuffle or choose option 2 & 3 or cancel
-         //also add options for y/n just not only yes/no
-         
-         System.out.print("Do you want to choose option 2, 3 or cancel account? ( 2, 3 ) or c to cancel: ");
-         String ans = prompt_user.nextLine();
-
-         if (ans.equals("2")) {
-            //implement second option
-            System.out.print("Are you sure to choose " + zero_seven_or_one + option + " ? (Yes / No) OR (y/n)" );
-         }
-         else if (ans.equals("3")) {
-            //implement third option
-         }
-         else {
-            //implement cancel account option
-         }
-        }
-
-        else{
-         System.out.println("Invalid choice, please try again: ");
-        }
 
     }//!=================================================================================================================
      
