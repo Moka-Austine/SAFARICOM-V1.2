0b2fdde6183aca687c32b0a1bbaaad80132d29ca
Moka-Austine <141416002+Moka-Austine@users.noreply.github.com>
2025-01-21 18:30:11 +0300

    Update App.java

--- SAFARICOM_APP/App.java
+++ SAFARICOM_APP/App.java
@@ -17,26 +17,30 @@ public class App {
         System.out.print("Do you have a Safaricom account? (Yes/No) OR (y/n)");
         String userAnswer = prompt_user.nextLine().toLowerCase();
 
-        //if user has an account
-         if (userAnswer.equals("yes") || userAnswer.equals("y")) {
-             System.out.println("\nEnter your username: ");
-             String userLoginUserName = prompt_user.nextLine();
+        switch(userAnswer) {
 
-             System.out.print("Enter your password: ");
-             String userLoginPassword = prompt_user.nextLine();
+             //?if user has an account========================================
+            case "yes", "y" -> { 
 
-             if (userLoginPassword.equals(PERSONAL_INFO.userInfor[1])) {
-                 System.out.println("\nWelcome back " + userLoginUserName + ". Thank you for choosing Safaricom again.");
-                 INFINITE_WHILE_LOOP.INFINITE_WHILE_LOOP(PERSONAL_INFO.userInfor, PERSONAL_INFO.agentInfor);
+                System.out.println("\nEnter your username: ");
+                String userLoginUserName = prompt_user.nextLine();
 
-              }
-         }
+                System.out.print("Enter your password: ");
+                String userLoginPassword = prompt_user.nextLine();
+                
+                if (userLoginPassword.equals(PERSONAL_INFO.userInfor[1])) {
+                    System.out.println("\nWelcome back " + userLoginUserName + ". Thank you for choosing Safaricom again.");
+                    INFINITE_WHILE_LOOP.INFINITE_WHILE_LOOP(PERSONAL_INFO.userInfor, PERSONAL_INFO.agentInfor);
+                    
+                }
+                break;
+            }
 
+            //? if user don't have an account or answer is no =========================================================
 
-        //? if user don't have an account=========================================================
-        else if (userAnswer.equals("no") || userAnswer.equals("n")){
-          
-            System.out.print("Do you want to open a Safaricom account? (Yes/No) OR (y/n)");
+            case "no", "n" -> {
+
+                System.out.print("Do you want to open a Safaricom account? (Yes/No) OR (y/n)");
             String user_answer = prompt_user.nextLine().toLowerCase();
 
             if (user_answer.equals("yes") || user_answer.equals("y")) {
@@ -45,44 +49,49 @@ public class App {
                   
                 String zero_seven = "07";
                 String zero_one = "01";
-                if(user_answer_after_choosing.equals("1")) {
-                    shuffling.shuffled_option_num(zero_seven, shuffling.shuffled_mobile_num1);
-                }
 
-                else if(user_answer_after_choosing.equals("2")) {
-                    shuffling.shuffled_option_num(zero_one, shuffling.shuffled_mobile_num2);
-                }
+                //String chosen_num;
+                switch(user_answer_after_choosing){
+                    case "1" -> shuffling.shuffled_option_num(zero_seven, shuffling.shuffled_mobile_num1);
+                        
+                    case "2" -> shuffling.shuffled_option_num(zero_one, shuffling.shuffled_mobile_num2);
 
-                else if(user_answer_after_choosing.equals("3")) {
-                    shuffling.shuffled_option_num(zero_seven, shuffling.shuffled_mobile_num3);
+                    case "3" -> shuffling.shuffled_option_num(zero_seven, shuffling.shuffled_mobile_num3);
+                        
+                    default -> System.out.println("");
+                        // keep on shuffling untill satisfied
+                        // also have a cancel option if not satisfied
                 }
-
-                else  {
-                    // keep on shuffling untill satisfied
-                    // also have a cancel option if not satisfied
-                }
-
                 
             }
+            
             else if (user_answer.equals("no") || user_answer.equals("n")) {
-                System.out.println("We hope to serve you in the future.");
+                System.out.println("Thank you for interacting with us. We hope to serve you in the future.");
                 
                 
             }
 
-        }
-        else {
-            System.out.println("Do you want to cancell subsciption? (Yes/No) OR (y/n)");
-            String subscription_confirm_answer = prompt_user.nextLine().toLowerCase();
-
-            if (subscription_confirm_answer.equals("yes") || subscription_confirm_answer.equals("y")) {
-                System.out.println("Thank you for interacting with our application, we hope to serve you in the future 😎.");
             }
 
-            else {
 
+            //?subscription cancellation
+            default -> {
+
+                    System.out.println("Do you want to cancell subsciption? (Yes/No) OR (y/n)");
+                    String subscription_confirm_answer = prompt_user.nextLine().toLowerCase();
+        
+                    if (subscription_confirm_answer.equals("yes") || subscription_confirm_answer.equals("y")) {
+                        System.out.println("Thank you for interacting with our application, we hope to serve you in the future 😎.");
+                    }
+        
+                    else {
+                      // answer is No
+                    }
+                
             }
+            
         }
+         
         prompt_user.close();
         
     }
